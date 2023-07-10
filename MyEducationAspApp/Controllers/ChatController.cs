2c92585aa13bcb482c1486abde6d50c236c8c52a
Zigguraptor <cobaltum8@gmail.com>
2023-07-11 02:21:41 +0400

    created chat page

---
+++ MyEducationAspApp/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyEducationAspApp.Models;
+
+namespace MyEducationAspApp.Controllers;
+
+public class ChatController : Controller
+{
+    private readonly ILogger<ChatController> _logger;
+
+    public ChatController(ILogger<ChatController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
