75f53c93c94aa62f8b19b02c7a92a971e634032d
Zigguraptor <cobaltum8@gmail.com>
2023-07-11 22:23:28 +0400

    Added VisitorsUpdate()

--- MyEducationAspApp/DAL/MainDbManager.cs
+++ MyEducationAspApp/DAL/MainDbManager.cs
@@ -15,4 +15,33 @@ public static class MainDbManager
             .OrderBy(m => m.TimeStamp)
             .ToList();
     }
+
+    public static void VisitorsUpdate(string ip, out int uniqueToDay, out int uniqueAllTime)
+    {
+        var dateNow = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        using var context = new MainDbContext(DbPath);
+        var visitor = context.Visitors.FirstOrDefault(entity => entity.IpAddress == ip);
+        if (visitor is null)
+        {
+            visitor = new VisitorEntity
+            {
+                IpAddress = ip,
+                VisitDate = dateNow
+            };
+            context.Visitors.Add(visitor);
+            context.SaveChanges();
+        }
+        else
+        {
+            if (visitor.VisitDate != dateNow)
+            {
+                visitor.VisitDate = dateNow;
+                context.SaveChanges();
+            }
+        }
+
+        uniqueAllTime = context.Visitors.Count();
+        uniqueToDay = context.Visitors.Count(v => v.VisitDate == dateNow);
+    }
 }
