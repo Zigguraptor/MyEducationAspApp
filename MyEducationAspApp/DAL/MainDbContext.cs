d6aa64f1a901907740afeca14b488675c9317a08
Zigguraptor <cobaltum8@gmail.com>
2023-07-11 18:14:34 +0400

    created MainDbContext.cs

---
+++ MyEducationAspApp/DAL/MainDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyEducationAspApp.DAL;
+
+public sealed class MainDbContext : DbContext
+{
+    public MainDbContext(string dbPath)
+    {
+        DbPath = dbPath;
+        Database.EnsureCreated();
+    }
+
+    public string DbPath { get; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
