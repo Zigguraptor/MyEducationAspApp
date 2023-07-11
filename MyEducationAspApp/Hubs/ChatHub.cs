67c506c6296f7e6fa13be336b2532a14ab421078
Zigguraptor <cobaltum8@gmail.com>
2023-07-11 18:33:55 +0400

    added writing message to db

--- MyEducationAspApp/Hubs/ChatHub.cs
+++ MyEducationAspApp/Hubs/ChatHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using JustAspEducationalProject;
+using Microsoft.AspNetCore.SignalR;
+using MyEducationAspApp.DAL;
+using MyEducationAspApp.DAL.Entities;
 
 namespace MyEducationAspApp.Hubs;
 
@@ -6,6 +9,14 @@ public class ChatHub : Hub
 {
     public async Task SendMessage(string user, string message)
     {
+        await using var dbContext = new MainDbContext(".\\MainDb.db");
+        dbContext.ChatMessages.Add(new ChatMessageEntity
+        {
+            TimeStamp = Time.GetTimeStamp(),
+            UserName = user,
+            MessageText = message
+        });
+        await dbContext.SaveChangesAsync();
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 }
