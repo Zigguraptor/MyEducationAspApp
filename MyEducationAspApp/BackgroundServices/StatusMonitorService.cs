223cfb5ed648ad824d05d73f11a6f617a1fc678e
Zigguraptor <cobaltum8@gmail.com>
2023-07-17 14:48:43 +0400

    added real stats getting

--- MyEducationAspApp/BackgroundServices/StatusMonitorService.cs
+++ MyEducationAspApp/BackgroundServices/StatusMonitorService.cs
@@ -1,33 +1,44 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.SignalR;
 using MyEducationAspApp.Hubs;
 
 namespace MyEducationAspApp.BackgroundServices;
 
 public class StatusMonitorService : BackgroundService
 {
+    private const string GetUsedRam = "free --kilo|awk 'NR==2 {print $3}'";
+    private const string GetUsedCpu = "vmstat 1 2|tail -1|awk '{print $15}'";
     private readonly ILogger<StatusMonitorService> _logger;
     private readonly IHubContext<StatusHub> _hubContext;
+    private readonly string _totalRam;
 
     public StatusMonitorService(ILogger<StatusMonitorService> logger, IHubContext<StatusHub> hubContext)
     {
         _logger = logger;
         _hubContext = hubContext;
+        var ram = double.Parse(BushExecutor.Execute("free --kilo|awk 'NR==2 {print $2}'"));
+        ram /= 1048576d; //ToGB
+        ram = Math.Round(ram, 2);
+        _totalRam = ram.ToString(CultureInfo.InvariantCulture);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var freeRam = GetFreeRam();
-            
-            await _hubContext.Clients.All.SendAsync("ReceiveStatus", freeRam, cancellationToken: stoppingToken);
-            
+            var stats = GetStats();
+
+            await _hubContext.Clients.All.SendAsync("ReceiveStatus", stats, cancellationToken: stoppingToken);
+
             await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
         }
     }
 
-    private string GetFreeRam()
+    private string GetStats()
     {
-        return "512 MB" + Random.Shared.NextInt64();
+        var usedRam = BushExecutor.Execute(GetUsedRam);
+        var usedCpu = BushExecutor.Execute(GetUsedCpu);
+
+        return $"Used CPU: {usedCpu}%\nUsed RAM: {usedRam} GB/{_totalRam} GB";
     }
 }
